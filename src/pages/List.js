0385b2cc31d436918d95e7429ff6ac4f613fe9f4
guimendes <guilherme@smarppy.com>
2023-06-26 14:34:08 -0300

    paginação

--- src/pages/List.js
+++ src/pages/List.js
@@ -5,6 +5,8 @@ import './List.css';
 
 const Listagem = () => {
   const [dados, setDados] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardsPerPage] = useState(40);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,11 +21,26 @@ const Listagem = () => {
     fetchData();
   }, []);
 
+  // Paginação
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = dados.slice(indexOfFirstCard, indexOfLastCard);
+  const totalPages = Math.ceil(dados.length / cardsPerPage);
+
+  // Funções para a paginação
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
   return (
     <div>
       <h1>Listagem</h1>
       <div className="card-list">
-        {dados.map((card) => (
+        {currentCards.map((card) => (
           <Link to={`/detalhes/${card.id}`} key={card.id} className="card-link">
             <div className="card">
               <img src={card.card_images[0].image_url} alt={card.name} className="card-image" />
@@ -31,6 +48,24 @@ const Listagem = () => {
             </div>
           </Link>
         ))}
+      </div>
+
+      {/* Paginação */}
+      <div className="pagination">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className="pagination-button"
+        >
+          Anterior
+        </button>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="pagination-button"
+        >
+          Próxima
+        </button>
       </div>
     </div>
   );
